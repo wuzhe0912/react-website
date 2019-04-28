99de68bc3375ab3f653ebcda1de3ca02001abb08
zhethon <kgb00128@gmail.com>
2019-04-28 22:38:55 +0800

    JSX

--- src/Welcome.js
+++ src/Welcome.js
@@ -2,9 +2,19 @@ import React from 'react'
 
 class Welcome extends React.Component {
   render() {
+    // const toDoList = ['Learn React', 'Learn Redux']
+    const isLogin = true
     return (
       <div>
         <h1> Hello, Pitt </h1>
+        {/* <ul>
+          {
+            toDoList.map(item =>
+              <li>{item}</li>
+            )
+          }
+        </ul> */}
+        { isLogin ? <p>已經登錄</p> : <p>請登錄</p> }
       </div>
     )
   }
