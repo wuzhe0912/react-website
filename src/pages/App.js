048fa497553a89c1eb92375a041a9eb96617bfec
Pitt Wu <kgb00128@gmail.com>
2020-08-17 12:05:15 +0800

    [feature]：新增 react-router 應用 header 切換頁面

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './styles/app.module.scss'
+import { Link, Route, withRouter } from 'react-router-dom';
+import HomePage from './HomePage';
+import WorkPage from './WorkPage';
+import WorkDetailPage from './WorkDetailPage';
 
-function App() {
-  return (
-    <div className={styles.App}>
-      <header className={styles.header}>
-        <div className={styles.header__wrap}>
-          <div className={styles.header__logo}>Pitt Website</div>
-          <div className={styles.header__list}></div>
-        </div>
-      </header>
-      <section className={styles.content}>
-      </section>
-      <footer className={styles.footer}>
-        <h4>&copy; 2020 Pitt Wu</h4>
-        <p>Handmade with</p>
-      </footer>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const { location } = this.props;
+    return (
+      <div className={styles.App}>
+        <header className={styles.header}>
+          <Link to="/" className={styles.header__logo}>Pitt Lab</Link>
+          <ul className={styles.header__list}>
+            <Link to="/" className={ location.pathname === '/' ? styles.selected : null }>HOME</Link>
+            <Link to="/works" className={ location.pathname === '/works' ? styles.selected : null }>WORK</Link>
+          </ul>
+        </header>
+        <section className={styles.content}>
+          <Route path="/" exact component={ HomePage }></Route>
+          <Route path="/works" exact component={ WorkPage }></Route>
+          <Route path="/works/:id" exact component={ WorkDetailPage }></Route>
+        </section>
+        <footer className={styles.footer}>
+          <h4>&copy; 2020 Pitt Wu</h4>
+          <p>Handmade with</p>
+        </footer>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default withRouter(App);
---
+++ src/pages/WorkDetailPage/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WorkDetailPage = () => {
+  return <div>Work Detail</div>
+}
+
+export default WorkDetailPage;
