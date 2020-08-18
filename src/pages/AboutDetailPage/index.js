bd6cd77e1a7fd15c08c677faea60a4ba7adc449a
Pitt Wu <kgb00128@gmail.com>
2020-08-18 23:59:18 +0800

    [feature]：新增 detail page

--- src/pages/AboutDetailPage/index.js
+++ src/pages/AboutDetailPage/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import styles from './detail.module.scss';
 
-const WorkDetailPage = () => {
-  return <div>Work Detail</div>
+export default () => {
+  return(
+    <div className={ styles.wrap }>
+      <div className={ styles.container }>
+        <div className={ styles.title }>
+          <h2>title</h2>
+        </div>
+        <section className={ styles.content }>
+          test text
+        </section>
+      </div>
+    </div>
+  )
 }
-
-export default WorkDetailPage;
